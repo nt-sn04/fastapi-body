80af0abf5d2cb4099af9746a42e7164367eb92b9
tursunov-backend <donitursunov98@gmail.com>
2026-03-26 18:38:51 +0500

    .

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 app = FastAPI(title='Request Body bilan ishlash')
 
+class Data(BaseModel):
+    a: float
+    b: float
+    operator: str
 
 @app.post('/api/products')
 async def create_product(
@@ -11,3 +16,19 @@ async def create_product(
     return data
 
 # POST /api/calculate {"a": 3, "b": 4, "operator": "+"} -> {"result": 7}
+
+
+@app.post('/api/calculate')
+async def calculate(data: Data):
+    if data.operator == '+':
+        result = data.a + data.b
+    elif data.operator == '-':
+        result = data.a - data.b
+    elif data.operator == '*':
+        result = data.a * data.b
+    elif data.operator == '/':
+        result = data.a / data.b
+    else:
+        return {"error": "Notogri operator"}
+    
+    return {"result": result}
